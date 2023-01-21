71e6ab73bbca8605daef606d4b3bc21457e4bb1f
Himanshu Dubey <112683056+himanshu24107@users.noreply.github.com>
2023-01-21 08:47:51 +0530

    Update 02.py

--- Sml Proj/Learn Python/Practical/02.py
+++ Sml Proj/Learn Python/Practical/02.py
@@ -1,5 +1,6 @@
-n1 = int(input("Enter first number: "))
-n2 = int(input("Enter Second number: "))
+#Program 2
+n1 = int(input("Enter first number: ")) #7
+n2 = int(input("Enter Second number: ")) #5
 
 def result():
     print(f'{x} is larger no. where as {y} is smaller.')
@@ -8,7 +9,7 @@ def result():
 if n1>=n2:
     if n1>n2:
         x = n1 
-        y = n2
+        y = n2 
         result()
     else:
         print("Both number are same.")
@@ -16,3 +17,4 @@ else:
     x = n2
     y = n1
     result()
+#Output: 7 is larger no. where as 5 is smaller
