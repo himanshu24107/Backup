07b451e323e55a562262305fa1c9cb429acbd7b9
Himanshu Dubey <112683056+himanshu24107@users.noreply.github.com>
2023-01-21 08:53:16 +0530

    Update 03.py

--- Sml Proj/Learn Python/Practical/03.py
+++ Sml Proj/Learn Python/Practical/03.py
@@ -1,13 +1,13 @@
+# Program 3
 lst = []
 def result():
-    print(f'{x} is largest')
-def addlst(n):
-    lst.append(n)
+    print(f'{x} is largest.')
+
 for i in range(0,3):
-    n = int(input("Enter numbers: "))
-    addlst(n)
+    n = int(input("Enter numbers: ")) #1, 2, 3
+    lst.append(n)
 
 lst.sort()
 x = lst[-1]
 result()
-
+#Output: 3 is largest.
