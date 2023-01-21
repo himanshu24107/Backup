6ec3aec8c24500b4ccc1ec00f8d6e16ab9ac087c
Himanshu Dubey <112683056+himanshu24107@users.noreply.github.com>
2023-01-21 09:13:36 +0530

    Update 05.py

--- Sml Proj/Learn Python/Practical/05.py
+++ Sml Proj/Learn Python/Practical/05.py
@@ -1,6 +1,7 @@
+# Program 5
 import math
-x = int(input("Enter base no.: "))
-n = int(input("Enter Degree: "))
+x = int(input("Enter base no.: ")) #5
+n = int(input("Enter Degree: ")) #2
 s = 0
 formula = "S = 1+x+x^2.......x^n"
 print(f'Formula: {formula}')
@@ -9,5 +10,5 @@ for i in range(0,n+1):
     j = math.pow(x,i)
     s = s+j
 print(f'Answer: {s}')
-
+# Output: Answer:31
 
