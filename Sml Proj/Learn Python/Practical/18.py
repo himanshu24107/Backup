d7e85634ad3ccaf1b5375248fb41c7cf575da717
Himanshu Dubey <112683056+himanshu24107@users.noreply.github.com>
2023-01-23 07:59:18 +0530

    Create 18.py

---
+++ Sml Proj/Learn Python/Practical/18.py
@@ -0,0 +1,17 @@
+#Program 18
+n = int(input("No. of elements in the list:"))
+search_num = int(input("Search no:"))
+lst = []
+for i in range(n):
+    k = int(input(f"Enter {i+1} element:"))
+    lst.append(k)
+reversed_list = []
+for i in range(len(lst)-1, -1, -1):
+    reversed_list.append(lst[i])
+print("Original list:", lst)
+print("Reversed list:", reversed_list)
+
+
+        
+
+
